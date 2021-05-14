e729ddfe0275843037ff27ed85f047617489b2ef
Naren <narayanan@welcometocbs.com>
2021-05-13 21:02:44 -0400

    commit

---
+++ node_app.js
@@ -0,0 +1,17 @@
+function sayHey(name) {
+    console.log('Hey World. This is a Node.js program by ' + name);
+}
+var sayHello = () => {
+    console.log("Say Hello World");
+}
+sayHey('Naren');
+sayHello();
+
+/* window.console.log("This is a window object");
+
+// var message = '';
+// globalThis.setTimeout(1); */
+
+
+
+console.log(module);
